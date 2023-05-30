6119d3fde04ebb5653de171c580f0c717acd2ed3
GoldenMEmre <goldenpie60@gmail.com>
2023-05-30 20:18:53 +0300

    Cucumber 3.gun

---
+++ src/test/java/runners/ParalelRunner1.java
@@ -0,0 +1,21 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"html:target/cucumber-reports1.html",//Rapor almak için bu classtan çalıştırmalıyız.
+                "json:target/json-reports/cucumber1.json",//(devam)cucumber-reports kısmı isteğe
+                "junit:target/xml-report/cucumber1.xml" // göre isimlendirilebilir
+        },
+        features = "src/test/resources/features",
+        glue = "stepdefinitions",
+        tags = "@ss",
+
+        dryRun = false//Testlerimizi çalıştırmak istediğimizde buradaki değer false olacak.
+)       //Eğer e
+public class ParalelRunner1 {
+}
